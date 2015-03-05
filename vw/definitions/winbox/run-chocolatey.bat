721a6c59b34a1d40e9505c18e013228bcbdc6d30
Tom Hensel <github@jitter.eu>
2015-03-05 21:06:42 +0100

    install Chocolatey

---
+++ vw/definitions/winbox/run-chocolatey.bat
@@ -0,0 +1,12 @@
+@REM https://chocolatey.org/packages/ChocolateyGUI
+choco install chocolateygui
+
+choco install atom
+choco install console2
+choco install duck
+choco install poshgit
+choco install procexp
+choco install putty
+choco install sysinternals
+
+EXIT 0
