d190726ebbdcebb02b62789fd17a30b661446073
Tom Hensel <github@jitter.eu>
2015-03-05 22:25:48 +0100

    development toolchain deluxe

--- vw/definitions/winbox/run-chocolatey.bat
+++ vw/definitions/winbox/run-chocolatey.bat
@@ -1,16 +1,54 @@
+@REM https://github.com/chocolatey/choco/wiki/CommandsReference
+
+
 @REM https://chocolatey.org/packages/ChocolateyGUI
-choco install chocolateygui
+choco install chocolateygui -ry
+
+
+@REM https://chocolatey.org/packages/ruby
+@REM let's stick with chef's embedded ruby for now (install-chef.bat)
+@REM  choco install ruby2 -ry
 
 @REM https://chocolatey.org/packages/ruby2.devkit
 @REM allows to update i.e. 'bigdecimal' in 'do-update_gems.bat' - will not fail though
-choco install ruby2.devkit
+choco install ruby2.devkit -ry
 
-choco install atom
-choco install console2
-choco install duck
-choco install poshgit
-choco install procexp
-choco install putty
-choco install sysinternals
+
+@REM https://chocolatey.org/packages/git
+choco install git --params="/GitAndUnixToolsOnPath /NoAutoCrlf" -ry
+
+
+@REM https://chocolatey.org/packages/Atom
+choco install atom -ry
+
+@REM https://chocolatey.org/packages/baretail
+choco install baretail -ry
+
+@REM https://chocolatey.org/packages/Console2
+choco install console2 -ry
+
+@REM https://chocolatey.org/packages/cyberduck
+choco install cyberduck -ry
+@REM https://chocolatey.org/packages/duck
+choco install duck -ry
+
+@REM https://chocolatey.org/packages/Firefox
+choco install firefox -ry
+
+@REM https://chocolatey.org/packages/logparser
+choco install logparser -ry
+
+@REM https://chocolatey.org/packages/procexp
+choco install procexp -ry
+
+@REM https://chocolatey.org/packages/putty.portable
+choco install putty.portable -ry
+
+@REM https://chocolatey.org/packages/SourceCodePro
+choco install sourcecodepro -ry
+
+@REM https://chocolatey.org/packages/sysinternals
+choco install sysinternals -ry
+
 
 EXIT 0
