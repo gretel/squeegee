87fa906ba96e3b146542055303eb8dd5a86dc533
Tom Hensel <github@jitter.eu>
2015-03-05 22:46:13 +0100

    fix paramater order (inspite documentation..)

--- vw/definitions/winbox/run-chocolatey.bat
+++ vw/definitions/winbox/run-chocolatey.bat
@@ -2,53 +2,53 @@
 
 
 @REM https://chocolatey.org/packages/ChocolateyGUI
-choco install chocolateygui -ry
+choco -ry install chocolateygui
 
 
 @REM https://chocolatey.org/packages/ruby
 @REM let's stick with chef's embedded ruby for now (install-chef.bat)
-@REM  choco install ruby2 -ry
+@REM  choco -ry install ruby2
 
 @REM https://chocolatey.org/packages/ruby2.devkit
 @REM allows to update i.e. 'bigdecimal' in 'do-update_gems.bat' - will not fail though
-choco install ruby2.devkit -ry
+choco -ry install ruby2.devkit
 
 
 @REM https://chocolatey.org/packages/git
-choco install git --params="/GitAndUnixToolsOnPath /NoAutoCrlf" -ry
+choco -ry install git --params="/GitAndUnixToolsOnPath /NoAutoCrlf"
 
 
 @REM https://chocolatey.org/packages/Atom
-choco install atom -ry
+choco -ry install atom
 
 @REM https://chocolatey.org/packages/baretail
-choco install baretail -ry
+choco -ry install baretail
 
 @REM https://chocolatey.org/packages/Console2
-choco install console2 -ry
+choco -ry install console2
 
 @REM https://chocolatey.org/packages/cyberduck
-choco install cyberduck -ry
+choco -ry install cyberduck
 @REM https://chocolatey.org/packages/duck
-choco install duck -ry
+choco -ry install duck
 
 @REM https://chocolatey.org/packages/Firefox
-choco install firefox -ry
+choco -ry install firefox
 
 @REM https://chocolatey.org/packages/logparser
-choco install logparser -ry
+choco -ry install logparser
 
 @REM https://chocolatey.org/packages/procexp
-choco install procexp -ry
+choco -ry install procexp
 
 @REM https://chocolatey.org/packages/putty.portable
-choco install putty.portable -ry
+choco -ry install putty.portable
 
 @REM https://chocolatey.org/packages/SourceCodePro
-choco install sourcecodepro -ry
+choco -ry install sourcecodepro
 
 @REM https://chocolatey.org/packages/sysinternals
-choco install sysinternals -ry
+choco -ry install sysinternals
 
 
 EXIT 0
